b77c48bfb03f9461f49f5451011e2c103608d2de
Hemanth Thambi <Hemanth.Thambi@ensarsolutions.com>
2025-05-20 18:47:22 +0530

    updated

--- src/lib/validations/lead.ts
+++ src/lib/validations/lead.ts
@@ -1,6 +1,5 @@
 
 import * as z from "zod";
-import { Status, Empcount } from "@/modules/leads/types";
 
 export const leadFormSchema = z.object({
   firstname: z.string().min(1, { message: "First name is required" }),
